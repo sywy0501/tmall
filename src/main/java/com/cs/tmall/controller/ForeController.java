68cb4a3916008be2bd75b76bd6cab1d395ad5227
sywy1 <chushi2017@outlook.com>
2018-01-10 16:08:17 +0800

    ssm仿天猫系统——首页

--- src/main/java/com/cs/tmall/controller/ForeController.java
+++ src/main/java/com/cs/tmall/controller/ForeController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.HtmlUtils;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 /**
@@ -24,6 +26,7 @@ import java.util.List;
 public class ForeController {
 
     public static String USERNAME_EXIST="用户名已经被使用,不能使用";
+    public static String WORNG_LOGIN="账号密码错误";
 
     @Autowired
     PropertyValueService propertyValueService;
@@ -64,5 +67,24 @@ public class ForeController {
         userService.add(user);
 
         return "redirect:registerSuccessPage";
+    }
+
+    @RequestMapping("forelogin")
+    public String login(@RequestParam("name")String name, @RequestParam("password")String password, Model model, HttpSession httpSession){
+        name = HtmlUtils.htmlEscape(name);
+        User user = userService.get(name,password);
+
+        if (null==user){
+            model.addAttribute("msg",WORNG_LOGIN);
+            return "fore/login";
+        }
+        httpSession.setAttribute("user",user);
+        return "redirect:forehome";
+    }
+
+    @RequestMapping("forelogout")
+    public String logout(HttpSession session){
+        session.removeAttribute("user");
+        return "redirect:forehome";
     }
 }
