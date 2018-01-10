5ff4bcd6cb88446f6d63eecc81d806df1f5f7c00
sywy1 <chushi2017@outlook.com>
2018-01-10 10:35:44 +0800

    ssm仿天猫系统

--- src/main/java/com/cs/tmall/pojo/Category.java
+++ src/main/java/com/cs/tmall/pojo/Category.java
@@ -1,9 +1,37 @@
 package com.cs.tmall.pojo;
 
+import java.util.List;
+
 public class Category {
     private Integer id;
 
     private String name;
+
+    /**
+     * 每个分类下的产品
+     **/
+    private List<Product> products;
+
+    /**
+     *  每个分类对应多个List<Product>，这个属性是为了在每个分类右边显示推荐产品列表
+     **/
+    private List<List<Product>> productsByRow;
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public List<List<Product>> getProductsByRow() {
+        return productsByRow;
+    }
+
+    public void setProductsByRow(List<List<Product>> productsByRow) {
+        this.productsByRow = productsByRow;
+    }
 
     public Integer getId() {
         return id;
