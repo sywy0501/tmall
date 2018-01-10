89feeb2739f6b4e2296c05d40f187b2a881f42df
sywy1 <chushi2017@outlook.com>
2018-01-10 11:14:13 +0800

    ssm仿天猫系统——首页Controller

--- src/main/java/com/cs/tmall/controller/ForeController.java
+++ src/main/java/com/cs/tmall/controller/ForeController.java
@@ -1,11 +1,13 @@
 package com.cs.tmall.controller;
 
 import com.cs.tmall.pojo.Category;
+import com.cs.tmall.pojo.User;
 import com.cs.tmall.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -20,6 +22,9 @@ import java.util.List;
 @Controller
 @RequestMapping
 public class ForeController {
+
+    public static String USERNAME_EXIST="用户名已经被使用,不能使用";
+
     @Autowired
     PropertyValueService propertyValueService;
     @Autowired
@@ -42,5 +47,22 @@ public class ForeController {
         productService.fillByRow(cs);
         model.addAttribute("cs",cs);
         return "fore/home";
+    }
+
+    @RequestMapping("foreregister")
+    public String register(Model model, User user){
+        String name = user.getName();
+        name = HtmlUtils.htmlEscape(name);
+        user.setName(name);
+        boolean exist = userService.isExist(name);
+
+        if (exist){
+            model.addAttribute("msg",USERNAME_EXIST);
+            model.addAttribute("user",null);
+            return "fore/register";
+        }
+        userService.add(user);
+
+        return "redirect:registerSuccessPage";
     }
 }
