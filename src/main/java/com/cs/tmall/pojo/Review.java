647973fa6870fa6a6b17d5b8c4571df4f652aaec
sywy1 <chushi2017@outlook.com>
2018-01-13 09:54:12 +0800

    ssm仿天猫系统——评价

--- src/main/java/com/cs/tmall/pojo/Review.java
+++ src/main/java/com/cs/tmall/pojo/Review.java
@@ -13,6 +13,16 @@ public class Review {
 
     private Date createDate;
 
+    private User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
     public Integer getId() {
         return id;
     }
