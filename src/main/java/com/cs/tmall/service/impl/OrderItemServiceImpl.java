90b09b2956792249bbeaf125ead1749165bd1b32
sywy1 <chushi2017@outlook.com>
2018-01-13 09:57:19 +0800

    ssm仿天猫系统

--- src/main/java/com/cs/tmall/service/impl/OrderItemServiceImpl.java
+++ src/main/java/com/cs/tmall/service/impl/OrderItemServiceImpl.java
@@ -83,6 +83,18 @@ public class OrderItemServiceImpl implements OrderItemService {
 
     }
 
+    @Override
+    public int getSaleCount(int pid) {
+        OrderItemExample example = new OrderItemExample();
+        example.createCriteria().andPidEqualTo(pid);
+        List<OrderItem> ois = orderItemMapper.selectByExample(example);
+        int result = 0;
+        for (OrderItem oi:ois){
+            result +=oi.getNumber();
+        }
+        return result;
+    }
+
     public void setProduct(List<OrderItem> ois){
         for (OrderItem oi: ois) {
             setProduct(oi);
